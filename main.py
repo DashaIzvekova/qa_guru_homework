9348dea2fbe3c8316d9ace2319227246af2ad71c
Daria Izvekova <dasha_13@bk.ru>
2026-06-27 09:25:47 +0500

    add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#1.Создайте словарь email
+email = {
+"subject": " Weekd plans ",
+    "from": "katya_yan@yandex. ",
+    "to": "frid@mail. ",
+    "body": "\tHey!\nLet's go hiking this weekd.\nBring snacks!\n"
+}
+
